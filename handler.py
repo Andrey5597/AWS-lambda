ea721246dad6dd20c68ade13fe2741b819b66445
andrey <shapovalandrey27@gmail.com>
2020-09-01 15:05:20 +0300

    initial

---
+++ handler.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import ast
+
+
+def load_timer(event, context):
+    """
+        example of request body: ["http://google.com", "http://facebook.com", "any fake url"]
+        response : {
+            statysCode: 200
+            "body": {
+            'url': 'load_time',
+            ...
+            }
+        }
+    """
+    try:
+        list_of_urls = ast.literal_eval(event.get('body'))
+    except Exception:   
+        return json.dumps({"statusCode" : 400})
+    body = {}
+    for url in list_of_urls:
+        try:
+            time = requests.get(url).elapsed.total_seconds()
+            body[url] = time
+        except Exception:
+            body[url] = 'URL is invalid'
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
